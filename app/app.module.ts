9e0ec12baade93a2d2ad19080eb43df6eaeab40e
ACoop <acooper@lrwonline.com>
2016-10-03 21:02:04 -0700

    before http

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { HeroesComponent } from './heroes.component';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { routing } from './app.routing';
+import { DashboardComponent } from './dashboard.component';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent
   ],
   bootstrap: [ 
     AppComponent 
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -3,11 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent }      from './heroes.component';
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 const appRoutes: Routes = [
   {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
   }
+
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
